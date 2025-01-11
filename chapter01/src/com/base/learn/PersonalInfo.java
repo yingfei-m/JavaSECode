79f4b0847b9b941147a72192ad4d388e7899647c
yingfei.mao <yingfei.mao@hotmail.com>
2025-01-11 22:52:01 +0800

    Initial commit

---
+++ chapter01/src/com/base/learn/PersonalInfo.java
@@ -0,0 +1,14 @@
+package com.base.learn;
+
+/**
+ * @Author myf15609
+ * @Date 2025/1/11
+ */
+public class PersonalInfo {
+    public static void main(String[] args) {
+        System.out.println("姓名：康师傅");
+        System.out.println();
+        System.out.println("性别：男");
+        System.out.println("家庭住址：北京程序员聚集地");
+    }
+}
